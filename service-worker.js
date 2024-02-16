256743e1e359667f45481e626557b68ed84e91fb
SaltyEgg <hzhou.me@saltyee.com>
2024-02-16 01:41:31 -0800

    a working sample

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,19 @@
-chrome.webRequest.onBeforeRequest.addListener(function (info) {
-        console.log("url: " + info.url);
-        return {redirectUrl: "https://www.google.com"};
-    },
-    // filters
-    {
-        urls: ["http://go/*", "https://go/*"],
-        types: ["main_frame"]
-    },
-    // extraInfoSpec
-    ["blocking"]
-);
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (isGoLink(tab)) {
+        const key = parseKey(tab.url)
+        chrome.tabs.update({url: "https://www.google.com?query=" + key});
+    }
+});
+
+
+const isGoLink = (tab) => {
+    if (!tab || !tab.url) return false
+
+    const url = tab.url.toLowerCase()
+    return url.startsWith('http://go/') || url.startsWith('https://go/')
+}
+
+const parseKey = (url) => {
+    const pathname = new URL(url).pathname
+    return pathname.split('/').pop()
+}
