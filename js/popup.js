9dbbd8aa216ebbfe496cb664ca858b0e7ac218d1
SaltyEgg <hzhou.me@saltyee.com>
2024-02-18 12:04:17 -0800

    enable notification display

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,31 @@
 const baseUrl = 'http://localhost:8888'
 
+const success = (message) => {
+    $.notify(message, "success")
+}
+const warn = (message) => {
+    $.notify(message, "warn")
+}
+const error = (message) => {
+    $.notify(message, "error")
+}
+const info = (message) => {
+    $.notify(message, "info")
+}
+const storeToken = (token, callback) => {
+    chrome.storage.local.set({token: token}, function () {
+        console.log('Value is set');
+        if (callback) callback()
+    });
+}
+
+const getToken = (callback) => {
+    chrome.storage.local.get(['token'], function (result) {
+        // todo: cleanup
+        console.log('Value currently is ' + JSON.stringify(result.token))
+        if (callback) callback(result.token)
+    });
+}
 const post = (url, data) => {
     return fetch(url, {
         method: 'POST',
@@ -20,34 +46,22 @@ const get = (url) => {
 }
 
 const requestEmailCode = (email) => {
-    post(baseUrl + '/email-code', {email: email})
+    post(baseUrl + '/email_code', {email: email})
 }
 
 const login = (email, code) => {
     console.log("login21")
     post(baseUrl + '/login', {email: email, code: code}).then(response => {
-        console.log("login")
         return response.json()
     }).then(data => {
         //TODO: cleanup
         console.log("token is " + JSON.stringify(data))
-        storeToken(data)
+        storeToken(data, () => {
+            showAfterLogin()
+        })
+    }).catch(error => {
+        console.error('Error:', error);
     })
-}
-
-const storeToken = (token, callback) => {
-    chrome.storage.local.set({token: token}, function () {
-        console.log('Value is set');
-        if (callback) callback()
-    });
-}
-
-const getToken = (callback) => {
-    chrome.storage.local.get(['token'], function (result) {
-        // todo: cleanup
-        console.log('Value currently is ' + JSON.stringify(result.token))
-        if (callback) callback(result.token)
-    });
 }
 
 const parseJwt = (token) => {
@@ -72,6 +86,25 @@ const isValidEmail = (email) => {
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         );
 };
+
+const refreshToken = (token) => {
+    post(baseUrl + '/refresh_token', token).then(response => {
+        return response.json()
+    }).then(data => {
+        storeToken(data)
+    })
+}
+
+const showAfterLogin = () => {
+    $(".no-login").hide()
+    $(".need-login").show()
+}
+
+const showBeforeLogin = () => {
+    $(".no-login").show()
+    $(".need-login").hide()
+}
+
 //---------------------------------
 
 $("#send-email-code").on("click", function () {
@@ -98,24 +131,27 @@ $("#login").on("click", function () {
 $("#create-new-link").on("click", function () {
     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
         let url = tabs[0].url
-        // use `url` here inside the callback because it's asynchronous!
+        let urlKey = $("input#url-key").val().trim();
+
+        if (urlKey.length === 0) {
+            alert("Please enter a valid go link key")
+            return
+        }
         alert(url)
     });
 });
 
 getToken((token) => {
     $('#spinner').hide()
+    // alert("token = " + JSON.stringify(token))
     if (token) {
-        if (isTokenExpired(token.access_token)) {
-            $(".no-login").show()
-            $(".need-login").hide()
-        } else {
-            $(".no-login").hide()
-            $(".need-login").show()
+        let isAccessExpired = isTokenExpired(token.access_token)
+        if (!isAccessExpired || (isAccessExpired && !isTokenExpired(token.refresh_token))) {
+            if (isAccessExpired) refreshToken(token)
+            showAfterLogin()
+            return
         }
-    } else {
-        $(".no-login").show()
-        $(".need-login").hide()
     }
+    showBeforeLogin()
 })
 
