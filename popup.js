0024589cd9026f15abf0da917ae5cc2e550bfcf0
SaltyEgg <hzhou.me@saltyee.com>
2024-02-18 00:29:13 -0800

    enable basic ui placeholder

--- popup.js
+++ popup.js
@@ -1,22 +1,62 @@
-// Component Hello
-function Hello() {
-    return `<div>You clicked {{self.number}} times</div>`;
+$("#create-new-link").on("click", function () {
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+        let url = tabs[0].url
+        // use `url` here inside the callback because it's asynchronous!
+        alert(url)
+    });
+});
+
+$(".no-login").show()
+
+$(".need-login").show()
+
+//----------------- Utils function -----------------
+
+const post = (url, data) => {
+    return fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
 }
 
-// Create the component
-function App() {
-    let self = this;
-    self.count = 1;
-    self.click = () => {
-        self.count++;
-    }
-    return `<>
-        <Hello :number="self.count" />
-        <input type="button" onclick="self.click" value="Click me" />
-    </>`;
+const get = (url) => {
+    return fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
 }
 
-// Register the dependencies to be used across the application
-lemonade.setComponents({Hello});
-// Render the component
-lemonade.render(App, document.getElementById('root'));
+const storeToken = (token, callback) => {
+    chrome.storage.local.set({token: token}, function () {
+        console.log('Value is set');
+        callback()
+    });
+}
+
+const getToken = (callback) => {
+    chrome.storage.local.get(['token'], function (result) {
+        console.log('Value currently is ' + result.token);
+        callback(result.token)
+    });
+}
+
+const parseJwt = (token) => {
+    let base64Url = token.split('.')[1];
+    let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    let jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+}
+
+const isTokenExpired = (token) => {
+    const jwt = parseJwt(token)
+    return jwt.exp < Date.now() / 1000
+}
+
