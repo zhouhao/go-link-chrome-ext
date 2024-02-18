4f4889b905d27418d26417d7be1fafbba7836b48
SaltyEgg <hzhou.me@saltyee.com>
2024-02-18 12:24:17 -0800

    enable context menu to clean local cache

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,11 +1,4 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (isGoLink(tab)) {
-        const key = parseKey(tab.url)
-        chrome.tabs.update({url: "https://www.google.com?query=" + key});
-    }
-});
-
-
+const RESET_LOCAL_CACHE = "RESET_LOCAL_CACHE"
 const isGoLink = (tab) => {
     if (!tab || !tab.url) return false
 
@@ -17,4 +10,27 @@ const parseKey = (url) => {
     const pathname = new URL(url).pathname
     return pathname.split('/').pop()
 }
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+        title: 'Reset Go Link local cache',
+        id: RESET_LOCAL_CACHE,
+        contexts: ['all'],
+    });
+})
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === RESET_LOCAL_CACHE) {
+        chrome.storage.local.clear(() => {
+        });
+    }
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (isGoLink(tab)) {
+        const key = parseKey(tab.url)
+        chrome.tabs.update({url: "https://www.google.com?query=" + key});
+    }
+});
+
 
