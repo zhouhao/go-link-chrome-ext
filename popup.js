694e9ed7665d18d5bccafbe50db673c41e0dce30
SaltyEgg <hzhou.me@saltyee.com>
2024-02-18 01:20:04 -0800

    can login now

--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+const baseUrl = 'http://localhost:8888'
+
 const post = (url, data) => {
     return fetch(url, {
         method: 'POST',
@@ -17,17 +19,33 @@ const get = (url) => {
     })
 }
 
+const requestEmailCode = (email) => {
+    post(baseUrl + '/email-code', {email: email})
+}
+
+const login = (email, code) => {
+    console.log("login21")
+    post(baseUrl + '/login', {email: email, code: code}).then(response => {
+        console.log("login")
+        return response.json()
+    }).then(data => {
+        //TODO: cleanup
+        console.log("token is " + JSON.stringify(data))
+        storeToken(data)
+    })
+}
+
 const storeToken = (token, callback) => {
     chrome.storage.local.set({token: token}, function () {
         console.log('Value is set');
-        callback()
+        if (callback) callback()
     });
 }
 
 const getToken = (callback) => {
     chrome.storage.local.get(['token'], function (result) {
-        console.log('Value currently is ' + result.token);
-        callback(result.token)
+        console.log('Value currently is ' + JSON.stringify(result.token))
+        if (callback) callback(result.token)
     });
 }
 
@@ -46,7 +64,35 @@ const isTokenExpired = (token) => {
     return jwt.exp < Date.now() / 1000
 }
 
+const isValidEmail = (email) => {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+};
 //---------------------------------
+
+$("#send-email-code").on("click", function () {
+    let email = $("input#user-email").val().trim();
+    if (!isValidEmail(email)) {
+        alert(email + " is not a valid email address")
+        return
+    }
+
+    requestEmailCode(email)
+});
+
+$("#login").on("click", function () {
+    let email = $("input#user-email").val().trim();
+    let emailCode = $("input#email-code").val().trim();
+    if (!isValidEmail(email) || emailCode.length === 0) {
+        alert("Please make sure email and code are valid")
+        return
+    }
+
+    login(email, emailCode)
+});
 
 $("#create-new-link").on("click", function () {
     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
@@ -59,7 +105,7 @@ $("#create-new-link").on("click", function () {
 getToken((token) => {
     $('#spinner').hide()
     if (token) {
-        if (isTokenExpired(token)) {
+        if (isTokenExpired(token.access_token)) {
             $(".no-login").show()
             $(".need-login").hide()
         } else {
